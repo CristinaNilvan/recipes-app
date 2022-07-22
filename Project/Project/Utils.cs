d81f269d1d047f01ecbc0d174991dc51d79eebff
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 17:49:40 +0300

    Add filter methods for calories and meal type

--- Project/Project/Utils.cs
+++ Project/Project/Utils.cs
@@ -8,6 +8,32 @@ namespace Project
 {
     internal class Utils
     {
+        public static List<Recipe> FilterByCalories(int calories, List<Recipe> recipes)
+        {
+            List<Recipe> filtered = new();
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (recipe.Calories <= calories)
+                    filtered.Add(recipe);
+            }
+
+            return filtered;
+        }
+
+        public static List<Recipe> FilterByMealType(string mealType, List<Recipe> recipes)
+        {
+            List<Recipe> filtered = new();
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (recipe.Type.MealType.Equals(mealType))
+                    filtered.Add(recipe);
+            }
+
+            return filtered;
+        }
+
         public static List<Recipe> FilterByServingTime(string servingTime, List<Recipe> recipes)
         {
             List<Recipe> filtered = new();
