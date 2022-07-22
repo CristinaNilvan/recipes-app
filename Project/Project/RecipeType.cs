f89a85d87f52239d6f3381fd0b6e076a448dd818
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 10:46:48 +0300

    Add RecipeType class

---
+++ Project/Project/RecipeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class RecipeType
+    {
+        private string? _mealType;
+        private string? _servingTime;
+    }
+}
