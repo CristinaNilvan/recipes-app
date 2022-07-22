d288c9b898814f3f5ec58e3c08c499e8ed6a5b97
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 10:44:15 +0300

    Add Recipe properties

--- Project/Project/Recipe.cs
+++ Project/Project/Recipe.cs
@@ -8,5 +8,12 @@ namespace Project
 {
     internal class Recipe
     {
+        private int _id;
+        private string? _name;
+        private string? _author;
+        private string? _description;
+        private string? _type;
+        private double _calories;
+        private List<Ingredient>? _ingredients;
     }
 }
