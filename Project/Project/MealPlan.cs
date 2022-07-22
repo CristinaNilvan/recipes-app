1299f1b32c07bed4047330c162f022720c7a4cb0
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 13:02:47 +0300

    Add MealPlan class

---
+++ Project/Project/MealPlan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class MealPlan
+    {
+        private List<Recipe>? _recipes;
+    }
+}
