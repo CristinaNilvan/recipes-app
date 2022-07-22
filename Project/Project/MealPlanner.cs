5a6375d12512f8ab746c15d2acda93d6833652ec
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 13:17:37 +0300

    Add method GenerateMealPlan

--- Project/Project/MealPlanner.cs
+++ Project/Project/MealPlanner.cs
@@ -8,5 +8,16 @@ namespace Project
 {
     internal class MealPlanner
     {
+        private List<Recipe>? _allRecipes;
+
+        public MealPlanner(List<Recipe> allRecipes)
+        {
+            _allRecipes = allRecipes;
+        }
+
+        public void GenerateMealPlan(string mealType, double calories)
+        {
+
+        }
     }
 }
