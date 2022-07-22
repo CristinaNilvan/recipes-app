e1a74c5bd33b34b56969285084927abf23c2e3b8
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 13:11:17 +0300

    Update MealPlan

--- Project/Project/MealPlan.cs
+++ Project/Project/MealPlan.cs
@@ -9,5 +9,14 @@ namespace Project
     internal class MealPlan
     {
         private List<Recipe>? _recipes;
+        private double calories;
+        
+        public MealPlan() 
+        {
+            _recipes = new List<Recipe>();
+        }
+
+        public void AddRecipe(Recipe recipe) => _recipes.Add(recipe); // + add calories
+        public void RemoveRecipe(Recipe recipe) => _recipes.Remove(recipe); // + remove calories
     }
 }
