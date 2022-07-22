0ef03ca247e23015f760f4b7713d0336a5105570
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 13:49:27 +0300

    Add Utils

---
+++ Project/Project/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Utils
+    {
+        public static List<Recipe> FilterByServingTime(string servingTime, List<Recipe> recipes)
+        {
+            List<Recipe> filtered = new();
+
+            foreach (Recipe recipe in recipes)
+            {
+                if (recipe.Type.ServingTime.Equals(servingTime))
+                    filtered.Add(recipe);
+            }
+
+            return filtered;
+        }
+    }
+}
