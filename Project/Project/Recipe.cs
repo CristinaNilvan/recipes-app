acbe980fb549d3f62ae34dca8a18474b35b38e8d
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-22 10:47:40 +0300

    Change type in Recipe

--- Project/Project/Recipe.cs
+++ Project/Project/Recipe.cs
@@ -12,7 +12,7 @@ namespace Project
         private string? _name;
         private string? _author;
         private string? _description;
-        private string? _type;
+        private RecipeType? _type;
         private double _calories;
         private List<Ingredient>? _ingredients;
     }
