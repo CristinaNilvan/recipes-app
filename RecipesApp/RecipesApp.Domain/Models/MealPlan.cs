3edd0ce5ed7bccb3c4e049121eba54250059a613
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-26 18:07:48 +0300

    Update MealPlan

--- RecipesApp/RecipesApp.Domain/Models/MealPlan.cs
+++ RecipesApp/RecipesApp.Domain/Models/MealPlan.cs
@@ -4,12 +4,36 @@
     {
         public MealPlan()
         {
-            
+            MealPlanDate = DateTime.Now;
         }
 
         public Recipe? Breakfast { get; set; }
         public Recipe? Lunch { get; set; }
         public Recipe? Dinner { get; set; }
-        public int Calories { get; set; }
+        public DateTimeOffset? MealPlanDate { get; set; }
+        public int Calories
+        {
+            get => Calories;
+
+            set => Calories = Breakfast.Calories + Lunch.Calories + Dinner.Calories;
+        }
+        public float Fats
+        {
+            get => Fats;
+
+            set => Fats = Breakfast.Fats + Lunch.Fats + Dinner.Fats;
+        }
+        public float Carbs
+        {
+            get => Carbs;
+
+            set => Carbs = Breakfast.Carbs + Lunch.Carbs + Dinner.Carbs;
+        }
+        public float Proteins
+        {
+            get => Proteins;
+
+            set => Proteins = Breakfast.Proteins + Lunch.Proteins + Dinner.Proteins;
+        }
     }
 }
