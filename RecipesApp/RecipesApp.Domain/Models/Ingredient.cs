c2fa190d08ffb307e0627a514cc063685a702f1b
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-26 17:36:34 +0300

    Add ingredient properties

--- RecipesApp/RecipesApp.Domain/Models/Ingredient.cs
+++ RecipesApp/RecipesApp.Domain/Models/Ingredient.cs
@@ -2,19 +2,22 @@
 {
     public class Ingredient
     {
-        public Ingredient(int id, string? name, string? type, int calories, double quantity)
+        public Ingredient(int id, string? name, IngredientCategory type, int calories, double quantity)
         {
             Id = id;
             Name = name;
-            Type = type;
+            Category = type;
             Calories = calories;
             Quantity = quantity;
         }
 
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Type { get; set; }
+        public IngredientCategory Category { get; set; }
         public int Calories { get; set; }
+        public float Fats { get; set; }
+        public float Carbs { get; set; }
+        public float Proteins { get; set; } 
         public double Quantity { get; set; }
     }
 }
