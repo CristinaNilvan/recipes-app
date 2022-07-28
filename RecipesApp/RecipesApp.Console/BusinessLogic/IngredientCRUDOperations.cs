9a74120464139de611a613d53baba2f92155fc7b
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 10:39:07 +0300

    Add CRUD classes

---
+++ RecipesApp/RecipesApp.Console/BusinessLogic/IngredientCRUDOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Console.BusinessLogic
+{
+    internal class IngredientCRUDOperations
+    {
+        public static Ingredient CreateIngredient(int id, string? name, IngredientCategory category, int calories, float fats,
+            float carbs, float proteins)
+        {
+            return new Ingredient(id, name, category, calories, fats, carbs, proteins);
+        }
+    }
+}
