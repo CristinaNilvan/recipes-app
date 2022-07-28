9bb49b332a3f5f0b878d95665e2edb265acc1c9f
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 12:58:55 +0300

    Add ListPrinter

---
+++ RecipesApp/RecipesApp.Console/BusinessLogic/ListPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Console.BusinessLogic
+{
+    internal class ListPrinter
+    {
+        public static void PrintList<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
