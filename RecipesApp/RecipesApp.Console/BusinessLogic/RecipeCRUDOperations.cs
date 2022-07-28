dacdf359d5dcf16ac84a96a9332092473f017c2e
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 11:24:38 +0300

    Update RecipeCRUDOperations

--- RecipesApp/RecipesApp.Console/BusinessLogic/RecipeCRUDOperations.cs
+++ RecipesApp/RecipesApp.Console/BusinessLogic/RecipeCRUDOperations.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RecipesApp.Domain.Models;
 
 namespace RecipesApp.Console.BusinessLogic
 {
-    internal class RecipeCRUDOperations
+    internal class RecipeCRUDOperations : BaseCRUDOperations<Recipe>
     {
+        public static Recipe Create()
+        {
+
+        }
     }
 }
