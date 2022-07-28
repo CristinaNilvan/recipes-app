520e1640cd34e997eb7712dab74f1d466c7883be
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 11:14:55 +0300

    Update

---
+++ RecipesApp/RecipesApp.Console/BusinessLogic/BaseCRUDOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Console.BusinessLogic
+{
+    internal class BaseCRUDOperations<T>
+    {
+        public static void Update(List<T> list, T firstElement, T secondElement)
+        {
+            int position = -1;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list.ElementAt(i).Equals(firstElement))
+                {
+                    position = i;
+                }
+            }
+
+            if (position > -1)
+            {
+                list.RemoveAt(position);
+                list.Add(secondElement);
+            }
+        }
+
+        public static void Delete(List<T> list, T element)
+        {
+            int position = -1;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list.ElementAt(i).Equals(element))
+                {
+                    position = i;
+                }
+            }
+
+            if (position > -1)
+                list.RemoveAt(position);
+        }
+    }
+}
--- RecipesApp/RecipesApp.Console/BusinessLogic/IngredientCRUDOperations.cs
+++ RecipesApp/RecipesApp.Console/BusinessLogic/IngredientCRUDOperations.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RecipesApp.Domain.Models;
+﻿using RecipesApp.Domain.Models;
 
 namespace RecipesApp.Console.BusinessLogic
 {
-    internal class IngredientCRUDOperations
+    internal class IngredientCRUDOperations : BaseCRUDOperations<Ingredient>
     {
-        public static Ingredient CreateIngredient(int id, string? name, IngredientCategory category, int calories, float fats,
+        public static Ingredient Create(int id, string? name, IngredientCategory category, int calories, float fats,
             float carbs, float proteins)
         {
             return new Ingredient(id, name, category, calories, fats, carbs, proteins);
