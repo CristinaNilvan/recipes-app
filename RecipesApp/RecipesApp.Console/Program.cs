69e7cae61e296f6e61ba1630f3d80502440853af
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 12:32:10 +0300

    Update Program

--- RecipesApp/RecipesApp.Console/Program.cs
+++ RecipesApp/RecipesApp.Console/Program.cs
@@ -1,25 +1,41 @@
 ﻿using RecipesApp.Domain.Models;
+using RecipesApp.Console.BusinessLogic;
 
-//Added for verification
-var ing01 = new Ingredient(1, "faina", 1, 1, 1, 1);
-var ing02 = new Ingredient(2, "caisa", 2, 2, 2, 2);
-var ing03 = new Ingredient(3, "ou", 3, 3, 3, 3);
+var entities = "1 - Ingredient; 2 - Recipe";
+var operations = "1 - Create; 2 - Update; 3 - Delete";
 
-var rec01 = new Recipe(1, "r1");
-rec01.AddIngredient(ing01);
+Console.WriteLine("Hello!");
 
-var rec02 = new Recipe(2, "r2");
-rec02.AddIngredient(ing02);
+while (true)
+{
+    Console.WriteLine("Continue to choose an entity? yes - 4, no - 5");
+    var continueEntity = Console.ReadLine();
 
-var rec03 = new Recipe(3, "r3");
-rec02.AddIngredient(ing03);
+    if (Convert.ToInt32(continueEntity) == 5)
+    {
+        break;
+    }
 
-var mealPlan = new MealPlan();
-mealPlan.Breakfast = rec01;
-mealPlan.Lunch = rec02;
-mealPlan.Dinner = rec03;
+    Console.WriteLine("Choose an entity: ");
+    Console.WriteLine(entities);
+    var chosenEntity = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Chosen entity: {chosenEntity}");
 
-mealPlan.CalculateCalories();
+    while (true)
+    {
+        Console.WriteLine("Continue to choose an operation? yes - 4, no - 5");
+        var continueOperation = Console.ReadLine();
 
-Console.WriteLine($"The meal plan has {mealPlan.Calories} calories.");
-Console.ReadKey();
+        if (Convert.ToInt32(continueOperation) == 5)
+        {
+            break;
+        }
+
+        Console.WriteLine("Choose an operation: ");
+        Console.WriteLine(operations);
+        var chosenOperation = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Chosen operation: {chosenOperation}");
+
+        HandleInput.HandleInputFromConsole(chosenEntity, chosenOperation);
+    }
+}
