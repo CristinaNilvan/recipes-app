69eb1b61f2a7cde5ef82c32382c8576316ea57d7
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-07-28 18:49:02 +0300

    Update Program

--- RecipesApp/RecipesApp.Console/Program.cs
+++ RecipesApp/RecipesApp.Console/Program.cs
@@ -2,16 +2,16 @@
 using RecipesApp.Console.InputHandling;
 
 var entities = "1 - Ingredient; 2 - Recipe";
-var operations = "1 - Create; 2 - Update; 3 - Delete";
+var operations = "1 - Create; 2 - Read; 3 - Update; 4 - Delete";
 
 Console.WriteLine("Hello!");
 
 while (true)
 {
-    Console.WriteLine("Continue to choose an entity? yes - 4, no - 5");
+    Console.WriteLine("Continue to choose an entity? yes - 5, no - 6");
     var continueEntity = Console.ReadLine();
 
-    if (Convert.ToInt32(continueEntity) == 5)
+    if (Convert.ToInt32(continueEntity) == 6)
     {
         break;
     }
@@ -23,10 +23,10 @@ while (true)
 
     while (true)
     {
-        Console.WriteLine("Continue to choose an operation? yes - 4, no - 5");
+        Console.WriteLine("Continue to choose an operation? yes - 5, no - 6");
         var continueOperation = Console.ReadLine();
 
-        if (Convert.ToInt32(continueOperation) == 5)
+        if (Convert.ToInt32(continueOperation) == 6)
         {
             break;
         }
