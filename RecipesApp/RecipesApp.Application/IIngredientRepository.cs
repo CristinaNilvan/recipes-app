e1d1b8b74ea0dbaaf4c1add8dd91591129163b36
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-02 15:14:45 +0300

    Add Repository interfaces

---
+++ RecipesApp/RecipesApp.Application/IIngredientRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application
+{
+    public interface IIngredientRepository
+    {
+        void CreateIngredient(Ingredient ingredient);
+        Ingredient GetIngredient(int ingredientId);
+        void UpdateIngredient(int ingredientId, Ingredient ingredient);
+        void DeleteIngredient(int ingredientId);
+        IEnumerable<Ingredient> GetIngredients();
+    }
+}
---
+++ RecipesApp/RecipesApp.Application/IRecipeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application
+{
+    public interface IRecipeRepository
+    {
+        void CreateRecipe(Recipe recipe);
+        Recipe GetRecipe(int recipeId);
+        void UpdateRecipe(int recipeId, Recipe recipe);
+        void DeleteRecipe(int recipeId);
+        void AddIngredientToRecipe(int recipeId, Ingredient ingredient);
+        void DeleteIngredientFromRecipe(int recipeId, Ingredient ingredient);
+        IEnumerable<Recipe> GetIngredients();
+    }
+}
