1b902467a0b535ab23fc851dd607a5a088f1e852
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-03 13:51:19 +0300

    Add SingleInMemoryIngredientRepository

---
+++ RecipesApp/RecipesApp.Assignments/CreationalPatterns/SingleInMemoryIngredientRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipesApp.Application;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Assignments.CreationalPatterns
+{
+    internal class SingleInMemoryIngredientRepository : IIngredientRepository
+    {
+        private static SingleInMemoryIngredientRepository _ingredientRepository;
+        private List<Ingredient> _ingredients = new List<Ingredient>();
+
+        private SingleInMemoryIngredientRepository()
+        {
+
+        }
+
+        public SingleInMemoryIngredientRepository Instance
+        {
+            get
+            {
+                if (_ingredientRepository == null)
+                    _ingredientRepository = new SingleInMemoryIngredientRepository();
+
+                return _ingredientRepository;
+            }
+
+            private set { }
+        }
+
+        public List<Ingredient> Ingredients => _ingredients;
+
+        public void CreateIngredient(Ingredient ingredient)
+        {
+            ingredient.Id = _ingredients.Count + 1;
+            _ingredients.Add(ingredient);
+        }
+
+        public void DeleteIngredient(int ingredientId)
+        {
+            var ingredient = _ingredients.FirstOrDefault(x => x.Id == ingredientId);
+            _ingredients.Remove(ingredient);
+        }
+
+        public Ingredient GetIngredient(int ingredientId)
+        {
+            return _ingredients.FirstOrDefault(x => x.Id == ingredientId);
+        }
+
+        public List<Ingredient> GetIngredients()
+        {
+            return _ingredients;
+        }
+
+        public void UpdateIngredient(int ingredientId, Ingredient newIngredient)
+        {
+            var ingredient = _ingredients.FirstOrDefault(x => x.Id == ingredientId);
+            var index = _ingredients.IndexOf(ingredient);
+            newIngredient.Id = ingredientId;
+            _ingredients[index] = newIngredient;
+        }
+    }
+}
