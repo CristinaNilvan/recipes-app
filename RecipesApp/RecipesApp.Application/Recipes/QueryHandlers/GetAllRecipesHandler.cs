ae07b8e92bf533e23f920e2dc057d0ac2da5234f
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-04 17:44:55 +0300

    Add query handlers for recipe

---
+++ RecipesApp/RecipesApp.Application/Recipes/QueryHandlers/GetAllRecipesHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Application.Abstractions;
+using RecipesApp.Application.Recipes.Queries;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.Recipes.QueryHandlers
+{
+    public class GetAllRecipesHandler : IRequestHandler<GetAllRecipes, List<Recipe>>
+    {
+        private readonly IRecipeRepository _repository;
+
+        public GetAllRecipesHandler(IRecipeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<Recipe>> Handle(GetAllRecipes request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_repository.GetAllRecipes());
+        }
+    }
+}
---
+++ RecipesApp/RecipesApp.Application/Recipes/QueryHandlers/GetRecipeByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Application.Abstractions;
+using RecipesApp.Application.Recipes.Queries;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.Recipes.QueryHandlers
+{
+    public class GetRecipeByIdHandler : IRequestHandler<GetRecipeById, Recipe>
+    {
+        private readonly IRecipeRepository _repository;
+
+        public GetRecipeByIdHandler(IRecipeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Recipe> Handle(GetRecipeById request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_repository.GetRecipeById(request.RecipeId));
+        }
+    }
+}
