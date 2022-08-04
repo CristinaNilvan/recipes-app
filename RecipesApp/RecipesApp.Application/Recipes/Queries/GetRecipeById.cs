0e629cdb143828c4c3a59242b78fe742b0e30bf4
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-04 17:33:52 +0300

    Add queries for recipe

---
+++ RecipesApp/RecipesApp.Application/Recipes/Queries/GetRecipeById.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.Recipes.Queries
+{
+    public class GetRecipeById : IRequest<Recipe>
+    {
+        public int RecipeId { get; set; }
+    }
+}
---
+++ RecipesApp/RecipesApp.Application/Recipes/Queries/GetRecipeByName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.Recipes.Queries
+{
+    public class GetRecipeByName : IRequest<Recipe>
+    {
+        public string RecipeName { get; set; }
+    }
+}
