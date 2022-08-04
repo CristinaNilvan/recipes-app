7818a7d56ca5977b76f7589665d59d8d7e07238a
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-04 17:07:11 +0300

    Update command

--- RecipesApp/RecipesApp.Application/Ingredients/Commands/UpdateIngredient.cs
+++ RecipesApp/RecipesApp.Application/Ingredients/Commands/UpdateIngredient.cs
@@ -18,6 +18,5 @@ namespace RecipesApp.Application.Ingredients.Commands
         public float Fats { get; set; }
         public float Carbs { get; set; }
         public float Proteins { get; set; }
-        public double Quantity { get; set; }
     }
 }
