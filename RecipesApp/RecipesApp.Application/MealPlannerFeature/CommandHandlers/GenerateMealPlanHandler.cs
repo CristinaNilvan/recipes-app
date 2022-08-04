b17c40a923bd4431915d3565e990272598c2dc2f
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-04 21:46:27 +0300

    Add command and command handler for meal planner

---
+++ RecipesApp/RecipesApp.Application/MealPlannerFeature/CommandHandlers/GenerateMealPlanHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Application.Abstractions;
+using RecipesApp.Application.MealPlannerFeature.Commands;
+using RecipesApp.Domain.Logic;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.MealPlannerFeature.CommandHandlers
+{
+    public class GenerateMealPlanHandler : IRequestHandler<GenerateMealPlan, MealPlan>
+    {
+        private readonly IMealPlanRepository _mealPlanRepository;
+        private readonly IRecipeRepository _recipeRepository;
+        private MealPlanner _mealPlanner;
+
+        public GenerateMealPlanHandler(IMealPlanRepository mealPlanRepository, IRecipeRepository recipeRepository)
+        {
+            _mealPlanRepository = mealPlanRepository;
+            _recipeRepository = recipeRepository;
+        }
+
+        public Task<MealPlan> Handle(GenerateMealPlan request, CancellationToken cancellationToken)
+        {
+            var recipes = _recipeRepository.GetAllRecipes();
+            var mealPlan = _mealPlanner.GenerateMealPlan(request.MealType, request.Calories);
+            _mealPlanRepository.CreateMealPlan(mealPlan);
+
+            return Task.FromResult(mealPlan);
+        }
+    }
+}
---
+++ RecipesApp/RecipesApp.Application/MealPlannerFeature/Commands/GenerateMealPlan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using RecipesApp.Domain.Enums;
+using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.MealPlannerFeature.Commands
+{
+    public class GenerateMealPlan : IRequest<MealPlan>
+    {
+        public MealType MealType { get; set; }
+        public int Calories { get; set; }
+    }
+}
