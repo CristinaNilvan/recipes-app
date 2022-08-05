855c77a0bcc59a8a364f163d501ffaed68e60b3b
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-05 14:43:48 +0300

    Update RecipeHandler : add mediators

--- RecipesApp/RecipesApp.Console/InputHandling/RecipeHandler.cs
+++ RecipesApp/RecipesApp.Console/InputHandling/RecipeHandler.cs
@@ -6,55 +6,23 @@ using System.Threading.Tasks;
 using RecipesApp.Domain.Enums;
 using RecipesApp.Domain.Models;
 using RecipesApp.Infrastructure.InMemoryRepositories;
+using MediatR;
+using RecipesApp.Application.Recipes.Commands;
+using RecipesApp.Application.Recipes.Queries;
+using RecipesApp.Domain.Enums;
+using RecipesApp.Domain.Models;
+using RecipesApp.Infrastructure.InMemoryRepositories;
 
 namespace RecipesApp.Console.InputHandling
 {
     internal class RecipeHandler
     {
-        private static InMemoryRecipeRepository _repository = new InMemoryRecipeRepository();
+        private static InMemoryRecipeRepository _repository = new InMemoryRecipeRepository(); //?
+        private static IMediator _mediator = MediatorSetup.GetMediatorForRecipe();
 
-        public static InMemoryRecipeRepository RecipeRepository => _repository;
+        public static InMemoryRecipeRepository RecipeRepository => _repository; //?
 
         public static void HandleCreateRecipe()
-        {
-            _repository.CreateRecipe(CreateRecipeFromInput());
-        }
-
-        public static void HandleReadRecipe()
-        {
-            System.Console.WriteLine("Please enter the id: ");
-            var id = Convert.ToInt32(System.Console.ReadLine());
-            System.Console.WriteLine(_repository.GetRecipeById(id));
-        }
-
-        public static void HandleUpdateRecipe()
-        {
-            HandleReadRecipes();
-
-            System.Console.WriteLine("Enter the id of the element you want to update: ");
-            var id = Convert.ToInt32(System.Console.ReadLine());
-            var recipe = CreateRecipeFromInput();
-
-            _repository.UpdateRecipe(id, recipe);
-        }
-
-        public static void HandleDeleteRecipe()
-        {
-            HandleReadRecipes();
-
-            System.Console.WriteLine("Enter the id of the element you want to delete: ");
-            var id = Convert.ToInt32(System.Console.ReadLine());
-
-            _repository.DeleteRecipe(id);
-        }
-
-        public static void HandleReadRecipes()
-        {
-            System.Console.WriteLine("Here are the current recipes: ");
-            ListPrinter.PrintList(_repository.Recipes);
-        }
-
-        private static Recipe CreateRecipeFromInput()
         {
             System.Console.WriteLine("Please enter the following data: ");
 
@@ -75,48 +43,90 @@ namespace RecipesApp.Console.InputHandling
             var servingTime = System.Console.ReadLine();
             var enumServingTime = (ServingTime)Enum.Parse(typeof(ServingTime), servingTime, true);
 
-            return new Recipe(name, author, description, enumMealType, enumServingTime, CreateIngredientList());
+            var ingredientList = InputHandlingUtils.CreateIngredientList();
+
+            _mediator.Send(new CreateRecipe()
+            {
+                Name = name,
+                Author = author,
+                Description = description,
+                MealType = enumMealType,
+                ServingTime = enumServingTime,
+                Ingredients = ingredientList
+            });
         }
 
-        private static List<Ingredient> CreateIngredientList()
+        public static async void HandleReadRecipe()
         {
-            var recipeIngredients = new List<Ingredient>();
+            System.Console.WriteLine("Please enter the id: ");
+            var id = Convert.ToInt32(System.Console.ReadLine());
 
-            System.Console.WriteLine("For the ingredients you can: ");
-
-            while (true)
+            var recipe = await _mediator.Send(new GetRecipeById()
             {
-                System.Console.WriteLine("1 - add from current ingredients");
-                System.Console.WriteLine("2 - create new ingredient");
-                var choice = Convert.ToInt32(System.Console.ReadLine());
+                RecipeId = id
+            });
 
-                if (choice == 1)
-                {
-                    IngredientHandler.HandleReadAllIngredients();
-                    System.Console.WriteLine("Enter the id of the element you want to add: ");
+            System.Console.WriteLine(recipe);
+        }
 
-                    var id = Convert.ToInt32(System.Console.ReadLine());
-                    var element = IngredientHandler.IngredientRepository.GetIngredientById(id);
+        public static async void HandleUpdateRecipe()
+        {
+            HandleReadAllRecipes();
 
-                    recipeIngredients.Add(element);
-                }
-                else if (choice == 2)
-                {
-                    var ingredient = IngredientHandler.CreateIngredientFromInput();
-                    recipeIngredients.Add(ingredient);
-                    IngredientHandler.IngredientRepository.CreateIngredient(ingredient);
-                }
+            System.Console.WriteLine("Enter the id of the element you want to update: ");
+            var id = Convert.ToInt32(System.Console.ReadLine());
 
-                System.Console.WriteLine("What do you want to do next? 1 - continue to add ingredients to recipe; 0 - exit");
-                var nextChoice = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("Please enter the following data: ");
 
-                if (nextChoice == 0)
-                {
-                    break;
-                }
-            }
+            System.Console.WriteLine("Name: ");
+            var name = System.Console.ReadLine();
 
-            return recipeIngredients;
+            System.Console.WriteLine("Author: ");
+            var author = System.Console.ReadLine();
+
+            System.Console.WriteLine("Decription: ");
+            var description = System.Console.ReadLine();
+
+            System.Console.WriteLine("Meal Type [Normal, Vegetarian, Vegan]: ");
+            var mealType = System.Console.ReadLine();
+            var enumMealType = (MealType)Enum.Parse(typeof(MealType), mealType, true);
+
+            System.Console.WriteLine("Serving Time [Breakfast, Lunch, Dinner]: ");
+            var servingTime = System.Console.ReadLine();
+            var enumServingTime = (ServingTime)Enum.Parse(typeof(ServingTime), servingTime, true);
+
+            var ingredientList = InputHandlingUtils.CreateIngredientList();
+
+            await _mediator.Send(new UpdateRecipe()
+            {
+                RecipeId = id,
+                Name = name,
+                Author = author,
+                Description = description,
+                MealType = enumMealType,
+                ServingTime = enumServingTime,
+                Ingredients = ingredientList
+            });
+        }
+
+        public static async void HandleDeleteRecipe()
+        {
+            HandleReadAllRecipes();
+
+            System.Console.WriteLine("Enter the id of the element you want to delete: ");
+            var id = Convert.ToInt32(System.Console.ReadLine());
+
+            await _mediator.Send(new DeleteRecipe()
+            {
+                RecipeId = id
+            });
+        }
+
+        public static async void HandleReadAllRecipes()
+        {
+            System.Console.WriteLine("Here are the current recipes: ");
+            var recipes = await _mediator.Send(new GetAllRecipes());
+            ListPrinter.PrintList(recipes);
         }
     }
 }
