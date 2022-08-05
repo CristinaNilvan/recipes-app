6124031bb1229ad14f083d1406b38a34e76998db
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-05 13:03:10 +0300

    Add MediatorSetup

---
+++ RecipesApp/RecipesApp.Console/InputHandling/MediatorSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using RecipesApp.Application.Abstractions;
+using RecipesApp.Infrastructure.InMemoryRepositories;
+
+namespace RecipesApp.Console.InputHandling
+{
+    internal class MediatorSetup
+    {
+        public static IMediator GetMediatorForIngredient()
+        {
+            var diContainer = new ServiceCollection()
+                .AddMediatR(typeof(IIngredientRepository))
+                .AddScoped<IIngredientRepository, InMemoryIngredientRepository>()
+                .BuildServiceProvider();
+
+            return diContainer.GetRequiredService<IMediator>();
+        }
+    }
+}
