d8585204e87c12fb8a21a48f61abb2b88c2e35f6
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-05 13:55:43 +0300

    Update IngredientHandler : delete unused method

--- RecipesApp/RecipesApp.Console/InputHandling/IngredientHandler.cs
+++ RecipesApp/RecipesApp.Console/InputHandling/IngredientHandler.cs
@@ -125,31 +125,5 @@ namespace RecipesApp.Console.InputHandling
             var ingredients = await _mediator.Send(new GetAllIngredients());
             ListPrinter.PrintList(ingredients);
         }
-
-        public static Ingredient CreateIngredientFromInput()
-        {
-            System.Console.WriteLine("Please enter the following data: ");
-
-            System.Console.WriteLine("Name: ");
-            var name = System.Console.ReadLine();
-
-            System.Console.WriteLine("Category [Meat, Dairy, Fruit, Vegetable, Herbs, Others]: ");
-            var category = System.Console.ReadLine();
-            var enumCategory = (IngredientCategory)Enum.Parse(typeof(IngredientCategory), category, true);
-
-            System.Console.WriteLine("Calories: ");
-            var calories = Convert.ToInt32(System.Console.ReadLine());
-
-            System.Console.WriteLine("Fats: ");
-            var fats = float.Parse(System.Console.ReadLine());
-
-            System.Console.WriteLine("Carbs: ");
-            var carbs = float.Parse(System.Console.ReadLine());
-
-            System.Console.WriteLine("Proteins: ");
-            var proteins = float.Parse(System.Console.ReadLine());
-
-            return new Ingredient(name, enumCategory, calories, fats, carbs, proteins);
-        }
     }
 }
