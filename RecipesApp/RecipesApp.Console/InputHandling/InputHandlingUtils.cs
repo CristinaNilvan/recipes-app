f3c39ef82964c8d004572f867bba9c95142f1664
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-05 13:55:04 +0300

    Add InputHandlingUtils

---
+++ RecipesApp/RecipesApp.Console/InputHandling/InputHandlingUtils.cs
@@ -0,0 +1,39 @@
+﻿using RecipesApp.Domain.Enums;
+using RecipesApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Console.InputHandling
+{
+    internal class InputHandlingUtils
+    {
+        public static Ingredient CreateIngredientFromInput()
+        {
+            System.Console.WriteLine("Please enter the following data: ");
+
+            System.Console.WriteLine("Name: ");
+            var name = System.Console.ReadLine();
+
+            System.Console.WriteLine("Category [Meat, Dairy, Fruit, Vegetable, Herbs, Others]: ");
+            var category = System.Console.ReadLine();
+            var enumCategory = (IngredientCategory)Enum.Parse(typeof(IngredientCategory), category, true);
+
+            System.Console.WriteLine("Calories: ");
+            var calories = Convert.ToInt32(System.Console.ReadLine());
+
+            System.Console.WriteLine("Fats: ");
+            var fats = float.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("Carbs: ");
+            var carbs = float.Parse(System.Console.ReadLine());
+
+            System.Console.WriteLine("Proteins: ");
+            var proteins = float.Parse(System.Console.ReadLine());
+
+            return new Ingredient(name, enumCategory, calories, fats, carbs, proteins);
+        }
+    }
+}
