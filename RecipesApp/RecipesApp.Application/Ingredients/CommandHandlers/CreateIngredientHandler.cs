d1b3018b035d7044b06256a853f26f64428b4cbc
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-08 13:41:31 +0300

    RecipesApp.Application : remove unused usings

--- RecipesApp/RecipesApp.Application/Ingredients/CommandHandlers/CreateIngredientHandler.cs
+++ RecipesApp/RecipesApp.Application/Ingredients/CommandHandlers/CreateIngredientHandler.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using RecipesApp.Application.Abstractions;
 using RecipesApp.Application.Ingredients.Commands;
 using RecipesApp.Domain.Models;
