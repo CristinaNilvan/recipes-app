3b9fbac96dc552f6ed09283e83e5fc5a1e5d7568
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-08 13:46:40 +0300

    RecipesApp.Infrastructure : remove unused usings

--- RecipesApp/RecipesApp.Infrastructure/PopulateLists.cs
+++ RecipesApp/RecipesApp.Infrastructure/PopulateLists.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RecipesApp.Domain.Enums;
 using RecipesApp.Domain.Models;
-using RecipesApp.Domain.Enums;
 
 namespace RecipesApp.Infrastructure
 {
