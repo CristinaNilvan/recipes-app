386e87e6f397ccce12788fc85d756e4ec0a8dcc8
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-08 13:43:27 +0300

    RecipesApp.Assignments : remove unused usings

--- RecipesApp/RecipesApp.Assignments/CreationalPatterns/FactoryMethod/InputHandlerFactory.cs
+++ RecipesApp/RecipesApp.Assignments/CreationalPatterns/FactoryMethod/InputHandlerFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipesApp.Assignments.CreationalPatterns.FactoryMethod
+﻿namespace RecipesApp.Assignments.CreationalPatterns.FactoryMethod
 {
     public class InputHandlerFactory
     {
--- RecipesApp/RecipesApp.Assignments/CreationalPatterns/Singleton/SingleInMemoryRecipeRepository.cs
+++ RecipesApp/RecipesApp.Assignments/CreationalPatterns/Singleton/SingleInMemoryRecipeRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RecipesApp.Application.Abstractions;
+﻿using RecipesApp.Application.Abstractions;
 using RecipesApp.Domain.Models;
 
 namespace RecipesApp.Assignments.CreationalPatterns.Singleton
