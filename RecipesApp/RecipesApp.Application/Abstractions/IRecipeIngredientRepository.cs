5aada4d2a61576c4d968641e0f94108811863c23
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-21 00:49:50 +0300

    Application.Abstractions : update and add repos

---
+++ RecipesApp/RecipesApp.Application/Abstractions/IRecipeIngredientRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecipesApp.Domain.Models;
+
+namespace RecipesApp.Application.Abstractions
+{
+    public interface IRecipeIngredientRepository
+    {
+        Task<RecipeIngredient> CreateRecipeIngredient(RecipeIngredient recipeIngredient);
+    }
+}
--- RecipesApp/RecipesApp.Application/Abstractions/IRecipeRepository.cs
+++ RecipesApp/RecipesApp.Application/Abstractions/IRecipeRepository.cs
@@ -4,13 +4,13 @@ namespace RecipesApp.Application.Abstractions
 {
     public interface IRecipeRepository
     {
-        Task CreateRecipe(Recipe recipe);
+        Task<Recipe> CreateRecipe(Recipe recipe, List<RecipeIngredient> recipeIngredients);
         Task<Recipe> GetRecipeById(int recipeId);
         Task<Recipe> GetRecipeByName(string recipeName);
         Task UpdateRecipe(Recipe newRecipe);
         Task UpdateRecipe(int recipeId, Recipe newRecipe);
         Task<Recipe> UpdateRecipeStatus(int recipeId, bool status);
-        Task DeleteRecipe(int recipeId);
+        Task<Recipe> DeleteRecipe(int recipeId);
         Task<List<Recipe>> GetAllRecipes();
         Task<List<Recipe>> GetRecipesByApprovedStatus(bool isApproved);
     }
