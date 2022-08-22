376aceb412e302e291fc2942d025a2ec16ea0270
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-22 18:25:33 +0300

    RecipesApp : define and implement unit of work

---
+++ RecipesApp/RecipesApp.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace RecipesApp.Application.Abstractions
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IIngredientRepository IngredientRepository { get; }
+        public IRecipeIngredientRepository RecipeIngredientRepository { get; }
+        public IRecipeRepository RecipeRepository { get; }
+        public IMealPlanRepository MealPlanRepository { get; }
+        Task Save();
+    }
+}
---
+++ RecipesApp/RecipesApp.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using RecipesApp.Application.Abstractions;
+using RecipesApp.Infrastructure.Context;
+
+namespace RecipesApp.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataContext _dataContext;
+
+        public UnitOfWork(DataContext dataContext, IIngredientRepository ingredientRepository,
+            IRecipeIngredientRepository recipeIngredientRepository, IRecipeRepository recipeRepository,
+            IMealPlanRepository mealPlanRepository)
+        {
+            _dataContext = dataContext;
+            IngredientRepository = ingredientRepository;
+            RecipeIngredientRepository = recipeIngredientRepository;
+            RecipeRepository = recipeRepository;
+            MealPlanRepository = mealPlanRepository;
+        }
+
+        public IIngredientRepository IngredientRepository { get; private set; }
+
+        public IRecipeIngredientRepository RecipeIngredientRepository { get; private set; }
+
+        public IRecipeRepository RecipeRepository { get; private set; }
+
+        public IMealPlanRepository MealPlanRepository { get; private set; }
+
+        public async Task Save()
+        {
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _dataContext.Dispose();
+        }
+    }
+}
