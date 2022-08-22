0b4affe6ba403d888792d1780ad0f3fa83436dac
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-08-22 21:08:15 +0300

    Assignments : add queries

---
+++ RecipesApp/RecipesApp.Assignments/Queries/IngredientStatsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesApp.Assignments.Queries
+{
+    internal class IngredientStatsDto
+    {
+        public string CategoryName { get; set; }
+        public int NumberOfIngredients { get; set; }
+
+        public override string ToString()
+        {
+            return $"Category : {CategoryName} ; Number of ingredients : {NumberOfIngredients}";
+        }
+    }
+}
---
+++ RecipesApp/RecipesApp.Assignments/Queries/Queries.cs
@@ -0,0 +1,48 @@
+﻿using RecipesApp.Infrastructure.Context;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace RecipesApp.Assignments.Queries
+{
+    public class Queries
+    {
+        public static async Task DoQueries()
+        {
+            await using var context = new DataContext();
+
+            /*Console.WriteLine("Paging recipes:");
+
+            var pageSize = 5;
+            var query1 = context
+                .Recipes
+                .OrderBy(x => x.Name)
+                .Take(pageSize)
+                .ToList();
+
+            ListPrinter.PrintList(query1);
+
+
+            Console.WriteLine("The maximum number of calories of an ingredient:");
+
+            var query2 = context
+                .Ingredients
+                .Max(x => x.Calories);
+
+            Console.WriteLine(query2);*/
+
+
+            Console.WriteLine("Ingredients grouped by type:");
+
+            var query3 = context
+                .Ingredients
+                .GroupBy(x => x.Category)
+                .Select(x => new IngredientStatsDto
+                {
+                    CategoryName = x.Key.ToString(),
+                    NumberOfIngredients = x.Count(),
+                })
+                .ToList();
+
+            ListPrinter.PrintList(query3);
+        }
+    }
+}
